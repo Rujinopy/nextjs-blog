4a2ec7c0f93967fdee1cfa6360ab563253ce115f
Rujinopy <ruth3442@gmail.com>
2022-09-22 19:29:11 +0700

    delete a single quote.

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Hi! I'm Ruji, a random guy learning nextjs lol</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
